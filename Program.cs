88d5fe05324e0188f1bf8953b09122a2d81897d4
Chris Hanson <chanson8@gmail.com>
2023-03-14 09:08:13 -0500

    setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace truthful_terrapins_heist_ii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
