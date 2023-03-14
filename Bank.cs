8e840034cebba7f24e281ba351bcef098444e316
Chris Hanson <chanson8@gmail.com>
2023-03-14 09:21:26 -0500

    bank settings done

---
+++ Bank.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace truthful_terrapins_heist_ii
+{
+  class Bank
+  {
+    public int CashOnHand { get; set; }
+    public int AlarmScore { get; set; }
+    public int VaultScore { get; set; }
+    public int SecurityGuardScore { get; set; }
+    public bool IsSecure
+    {
+      get
+      {
+        return !(AlarmScore <= 0 && VaultScore <= 0 && SecurityGuardScore <= 0);
+      }
+    }
+  }
+}
